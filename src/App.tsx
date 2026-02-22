0bb653fc091f51100b6a3804ba972766cc3334a1
Irwing <irwing_d@yahoo.co.in>
2026-02-22 11:44:54 +0530

    tried to fix offset issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import PongCanvas from "./components/PongCanvas"
 
 function App() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: 40 }}>
-      <PongCanvas />
-    </div>
-  )
+  return <PongCanvas />
 }
-
 export default App
